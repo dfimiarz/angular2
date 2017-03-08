4cc6a5d6d87aa8e84f40b92bd3e272b0f98b3d1b
dfimiarz <dfimiarz@ccny.cuny.edu>
2017-03-08 16:05:24 -0500

    Completed part 1 tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,27 @@
 import { Component } from '@angular/core';
 
-export class Hero{
-  id: number;
-  name: string;
-}
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
+import { TodoDataService } from './todo-data.service'
+import { Todo } from './todo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [TodoDataService]
 })
 export class AppComponent {
+
   title = 'CURES';
-  heroes = HEROES;
+  newTodo: Todo = new Todo();
+
+  constructor(private todoDataService: TodoDataService) { }
+
+  addTodo() {
+    this.todoDataService.addTodo(this.newTodo);
+    this.newTodo = new Todo();
+  }
+
+  get todos(){
+    return this.todoDataService.getAllTodos();
+  }
 }
